d6c6d2c814ea6ee09feca0a0f386f3d2a1a39192
Rdlesser <rdlesser@gmail.com>
2021-06-16 18:04:11 +0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using GameAnalyticsSDK;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour, IGameAnalyticsATTListener
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            GameAnalytics.RequestTrackingAuthorization(this);
+        }
+        else
+        {
+            GameAnalytics.Initialize();
+        }
+    }
+    
+    public void GameAnalyticsATTListenerNotDetermined()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerRestricted()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerDenied()
+    {
+        GameAnalytics.Initialize();
+    }
+    public void GameAnalyticsATTListenerAuthorized()
+    {
+        GameAnalytics.Initialize();
+    }
+}
